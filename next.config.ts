9031bff9c090fe6eba2cc2974d78a78181257e7e
Александр <taiko6661@gmail.com>
2025-02-17 18:07:16 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/moysklad-testtask",
   output: "export", // <=== enables static exports
+  ignoreBuildErrors: true,
 };
 
 export default nextConfig;
